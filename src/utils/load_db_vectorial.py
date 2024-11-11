802bc3d0cb057061c12b18d51dd5052bae44bdb5
Georffrey Arevalo <georffreyarevalo@gmail.com>
2024-11-11 11:33:56 -0500

    Final App

--- src/utils/load_db_vectorial.py
+++ src/utils/load_db_vectorial.py
@@ -18,8 +18,7 @@ class VectorialDB:
             os.environ['PINECONE_API_KEY'] = envs()['API_KEY_PINECONE']
             pc = Pinecone( api_key=envs()['API_KEY_PINECONE'] )
             VectorialDB.INDEX_NAME = envs()['INDEX_NAME_PINECONE']
-            # model_id_embeddings = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
-            model_id_embeddings = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
+            model_id_embeddings = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
             VectorialDB.embeddings = HuggingFaceEmbeddings( model_name=model_id_embeddings )
             VectorialDB.vstore = PineconeVectorStore.from_existing_index(VectorialDB.INDEX_NAME, VectorialDB.embeddings)
         return [VectorialDB.vstore, VectorialDB.embeddings, VectorialDB.INDEX_NAME]
